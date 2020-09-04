f98fb680ae8a098023dfa1ba1bde629cb0efbe43
carlosahs <tacahs12@gmail.com>
2020-09-04 14:26:10 -0500

    Add Note component to index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './App';
+import Note from './Note';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Note />
   </React.StrictMode>,
   document.getElementById('root')
 );
