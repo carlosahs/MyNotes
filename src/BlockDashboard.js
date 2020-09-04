4f9465a8c3b2cca713d615f55fc7473dac6ef42f
carlosahs <tacahs12@gmail.com>
2020-09-04 15:05:34 -0500

    Add all required components

---
+++ src/BlockDashboard.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Block from "./Block";
+import BlockList from "./BlockList";
+
+class BlockDashboard extends React.Component {
+  state = {
+    blocks: [],
+  };
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
