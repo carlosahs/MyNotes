c11b15e3b72000adcf467228dd3c638418114985
carlosahs <tacahs12@gmail.com>
2020-09-07 18:45:40 -0500

    Refactor dummy block data

--- src/BlockDashboard.js
+++ src/BlockDashboard.js
@@ -1,47 +1,27 @@
 import React from "react";
 
+import { v4 as uuidv4 } from "uuid";
+// React components
 import Block from "./Block";
 import BlockAdder from "./BlockAdder";
 import BlockContainer from "./BlockContainer"
 import BlockList from "./BlockList";
 
-const dummyBlocks = [
-  <BlockContainer
-    key={1}
-    type="math"
-  />,
-  <BlockContainer 
-    key={2}
-    type="code"
-  />,
-  <BlockContainer 
-    key={3}
-    type="heading"
-  />,
-  <BlockContainer
-    key={4}
-  />, // default type: text
-  <BlockContainer 
-    key={5}
-    type="table"
-  />,
-  <BlockContainer
-    key={6}
-    type="srs"
-  />,
-  <BlockContainer
-    key={7}
-    type="image"
-  />,
-  <BlockContainer
-    key={8}  
-    type="sound"
-  />,
-  <BlockContainer
-    key={9}
-    type="graphics"
-  />
+const blockTypes = [
+  "math", "code", "heading", 
+  "text", "table", "srs", 
+  "image", "sound", "graphics"
 ];
+
+const blockData = blockTypes.map(type => {
+  return {
+    "id": uuidv4(),
+    "type": type,
+    "content": "",
+  };
+});
+
+const dummyBlocks = "";
 
 class BlockDashboard extends React.Component {
   state = {
