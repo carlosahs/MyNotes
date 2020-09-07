2cdb31c62ef9371d7b6a2612e7b0c53b50d65c35
carlosahs <tacahs12@gmail.com>
2020-09-07 17:49:26 -0500

    Add dummy block array

--- src/BlockDashboard.js
+++ src/BlockDashboard.js
@@ -1,7 +1,39 @@
 import React from "react";
 
 import Block from "./Block";
+import BlockAdder from "./BlockAdder";
 import BlockList from "./BlockList";
+
+const dummyBlocks = [
+  <Block 
+    type="math"
+  />,
+  <Block 
+    type="code"
+  />,
+  <Block 
+    type="heading"
+  />,
+  <Block />, // default type: text
+  <Block 
+    type=""
+  />,
+  <Block 
+    type="table"
+  />,
+  <Block
+    type="srs"
+  />,
+  <Block
+    type="image"
+  />,
+  <Block  
+    type="sound"
+  />,
+  <Block
+    type="graphics"
+  />
+];
 
 class BlockDashboard extends React.Component {
   state = {
@@ -9,12 +41,17 @@ class BlockDashboard extends React.Component {
   };
 
   componentDidMount() {
-
+    this.setState({ blocks: dummyBlocks });
   }
 
   render() {
     return (
-      <div></div>
+      <div>
+        <BlockList
+          blocks = {this.state.blocks}
+        />
+        <BlockAdder />
+      </div>
     );
   }
 }
