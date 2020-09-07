94b8eb507511f2b021347af98421846739aa20e6
carlosahs <tacahs12@gmail.com>
2020-09-07 17:53:41 -0500

    Add momentary keys to items

--- src/BlockDashboard.js
+++ src/BlockDashboard.js
@@ -6,28 +6,38 @@ import BlockList from "./BlockList";
 
 const dummyBlocks = [
   <Block 
+    key={1}
     type="math"
   />,
   <Block 
+    key={2}
     type="code"
   />,
   <Block 
+    key={3}
     type="heading"
   />,
-  <Block />, // default type: text
+  <Block
+    key={4}
+  />, // default type: text
   <Block 
+    key={5}
     type="table"
   />,
   <Block
+    key={6}
     type="srs"
   />,
   <Block
+    key={7}
     type="image"
   />,
-  <Block  
+  <Block
+    key={8}  
     type="sound"
   />,
   <Block
+    key={9}
     type="graphics"
   />
 ];
